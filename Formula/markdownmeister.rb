9d1d925f9d1034b6b9d68e94eac1853692b8796d
yetanotherchris <54912+yetanotherchris@users.noreply.github.com>
2026-08-19 22:35:02 +0000

    chore(release): update package definitions to v1.0.2

--- Formula/markdownmeister.rb
+++ Formula/markdownmeister.rb
@@ -1,16 +1,16 @@
 class Markdownmeister < Formula
   desc "A WYSIWYG markdown editor for Windows, macOS, and Linux, built with Electron and Milkdown."
   homepage "https://github.com/yetanotherchris/markdownmeister"
-  version "1.0.0"
+  version "1.0.2"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/yetanotherchris/markdownmeister/releases/download/v1.0.0/markdownmeister-1.0.0-macos-arm64.zip"
-      sha256 "b9c7120cee15b42fc5155f38f7bdd980718fec0045a3f01538973adab81ed9ef"
+      url "https://github.com/yetanotherchris/markdownmeister/releases/download/v1.0.2/markdownmeister-1.0.2-macos-arm64.zip"
+      sha256 "9896e2624b945a8dcef0bbccfb306311752b8322d8f41dee23d4e01a4014797b"
     else
-      url "https://github.com/yetanotherchris/markdownmeister/releases/download/v1.0.0/markdownmeister-1.0.0-macos-x64.zip"
-      sha256 "2f7669d7d43c37eec2f49002050192a51c0c9f89244a00afd2701611b8575ad9"
+      url "https://github.com/yetanotherchris/markdownmeister/releases/download/v1.0.2/markdownmeister-1.0.2-macos-x64.zip"
+      sha256 "d80dcd55cbce4e4bc44ba08376aed714391f731b85c21ae8162714f9b58d040c"
     end
   end
 
@@ -18,8 +18,8 @@ class Markdownmeister < Formula
     if Hardware::CPU.arm?
       odie "MarkdownMeister does not provide a Linux arm64 build"
     else
-      url "https://github.com/yetanotherchris/markdownmeister/releases/download/v1.0.0/markdownmeister-1.0.0-linux-x64.AppImage"
-      sha256 "16d853122abed6ffe0c13621bbc5c20d0f1331fffa7592557805580b5aa74055"
+      url "https://github.com/yetanotherchris/markdownmeister/releases/download/v1.0.2/markdownmeister-1.0.2-linux-x64.AppImage"
+      sha256 "7fce30e4633473cc042712e8ea2415bb83015a9b05d9eac7ab219b9a41e8c8ab"
     end
   end
 
@@ -27,7 +27,7 @@ class Markdownmeister < Formula
     if OS.mac?
       app.install "MarkdownMeister.app"
     else
-      bin.install "markdownmeister-1.0.0-linux-x64.AppImage" => "markdownmeister"
+      bin.install "markdownmeister-1.0.2-linux-x64.AppImage" => "markdownmeister"
     end
   end
 
